04663bd92b958a61dd4343f03057080b46ea3487
Josimar Galeano <ing.galeano24@gmail.com>
2024-04-22 11:45:12 -0500

    Adjust java versions on the data and domain modules

--- app/src/main/java/com/jgaleano/pokedex_compose/MainActivity.kt
+++ app/src/main/java/com/jgaleano/pokedex_compose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jgaleano.pokedex_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -14,7 +15,9 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
         setContent {
             PokedexComposeTheme {
                 // A surface container using the 'background' color from the theme
